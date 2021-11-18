6f3b0800328cd2f5976459c512fbfd5c6aa59172
nicolas jolys <jolys.nicolas@gmail.com>
2021-11-18 14:41:59 +0100

    Prepare rails app with external specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,23 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)require 'faker'
+
+# require 'faker'
+
+
+# puts "Cleaning database..."
+# # Movie.destroy_all
+
+# puts "Creating 12 movies..."
+
+# 12.times do
+#   movie = Movie.new(
+#     title:    Faker::Movie.title,
+#     overview: Faker::Movies::BackToTheFuture.quote,
+#     poster_url: Faker::Internet.url(host: 'movies.com'),
+#     rating: rand(5..10).to_f,
+#   )
+#   movie.save!
+# end
+# puts 'Finished!'
