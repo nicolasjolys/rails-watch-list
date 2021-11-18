00806ea8623b22f101475943e32e6c0b4c37e337
nicolas jolys <jolys.nicolas@gmail.com>
2021-11-18 14:03:51 +0100

    message automatique

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  # You can’t delete a movie if it is referenced in at least one bookmark.
+
+
+  validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
+end
